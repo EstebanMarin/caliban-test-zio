68d276801089ad0ba43f5a9e16cfa05df52ffae3
EstebanMarin <estebanmarin002@gmail.com>
2025-02-22 13:20:18 -0500

    almost there

--- project.scala
+++ project.scala
@@ -4,6 +4,13 @@
 //> using dep "dev.zio::zio-logging:2.4.0"
 //> using dep "dev.zio::zio-logging-slf4j2:2.4.0"
 //> using dep "com.github.ghostdogpr::caliban:2.9.2"
+//> using dep "com.github.ghostdogpr::caliban-zio-http:2.9.2"
+//> using dep "com.github.ghostdogpr::caliban-federation:2.9.2"
 //> using dep "com.github.ghostdogpr::caliban-quick:2.9.2"
+//> using dep "com.github.ghostdogpr::caliban-client:2.9.2"
 //> using dep "org.slf4j:slf4j-simple:2.0.16"
+//> using dep "com.softwaremill.sttp.tapir::tapir-json-circe:1.11.15"
+//> using dep "com.softwaremill.sttp.client3::zio:3.9.0"
 
+// Make usre to grab these?
+// https://github.com/ghostdogpr/caliban/blob/series/2.x/examples/README.md
